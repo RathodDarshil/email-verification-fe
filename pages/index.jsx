a22ab23c897cf743cb9a27a9f9f75787874d4a6d
RathodDarshil <darshilrathod0@gmail.com>
2021-08-07 23:56:16 +0530

    build issue fixed

--- pages/index.jsx
+++ pages/index.jsx
@@ -100,7 +100,6 @@ export default function Home() {
                                 type="email"
                                 placeholder="Enter email"
                             />
-                            <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
